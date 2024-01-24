acb9eec25b3d352d7da63791d0649da893e6a8bc
Stéphane Francel <stephane.francel@gmail.com>
2024-01-24 16:39:07 +0100

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { JsonPipe, NgClass } from '@angular/common';
+import { AfterViewChecked, AfterViewInit, Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [JsonPipe, RouterOutlet, FormsModule, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild(NgForm) form!: NgForm;
+
+  ngAfterViewInit(): void {
+    this.form.statusChanges?.subscribe(console.log);
+  }
+
+  submit(value: any) {
+
+    console.log(this.form);
+
+
+    console.log(value);
+  }
+}
